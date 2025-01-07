48bd9fc640488f08f40c79382d8e988f47fd3c1b
Julian Bias <julian_bias@yahoo.com>
2025-01-07 17:55:26 -0600

    fixed protected route for auth

--- client/components/ProtectedRoute.jsx
+++ client/components/ProtectedRoute.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
 
 const ProtectedRoute = ({ element, isAuthenticated }) => {
+  const { isAuthenticated } = useContext(AuthContext);
   return isAuthenticated ? element : <Navigate to="/login" />;
 };
 
